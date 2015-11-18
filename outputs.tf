5b2d8abbc5eeb723784998e134e55c05d8b404f9
Baskaran <baskaran.md@gmail.com>
2015-11-18 12:09:34 -0800

    Fix outputs.tf for aws_spot_instance_request

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,16 @@
 // Output the ID of the EC2 instance created
-output "ec2_instance_id" {
-  value = "${aws_instance.ec2_instance.id}"
+output "instance_id" {
+  value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.id)}"
+}
+
+output "spot_request_state" {
+  value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.spot_request_state)}"
+}
+
+output "spot_instance_id" {
+  value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.spot_instance_id)}"
+}
+
+output "spot_bid_status" {
+  value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.spot_bid_status)}"
 }
