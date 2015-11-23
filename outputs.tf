a606c0cdcd1efc3cdb1397fba04bebbea36569b9
Baskaran <baskaran.md@gmail.com>
2015-11-23 14:44:09 -0800

    Add Private IPs in output of terraform spot instances request

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "instance_id" {
   value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.id)}"
 }
 
+// Since Terraform v0.6.7
+output "private_ips" {
+    value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.private_ip)}"
+}
+
 output "spot_request_state" {
   value = "${join(",", aws_spot_instance_request.ec2_spot_instance.*.spot_request_state)}"
 }
